50eac243c84b5cfc6510b30b6bce9b740c885cb1
moigabone <moigab.one@gmail.com>
2024-12-12 23:29:18 +0100

    commit_test

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,14 @@
-import { OrbitControls } from "@react-three/drei";
+import { ScrollControls, useScroll, OrbitControls } from "@react-three/drei";
 import { Office } from "./Office";
 
 export const Experience = () => {
   return (
     <>
-      <OrbitControls />
       <ambientLight intensity={1}/>
+      <OrbitControls enableZoom={false} />
+      <ScrollControls pages={3} damping={0.25}>
       <Office />
+      </ScrollControls>
     </>
   );
 };
